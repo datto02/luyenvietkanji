c1a8bab1137b0f9cbab7b9fea52ecf87d8c886f6
datto02 <vuthedat333@gmail.com>
2026-04-05 00:15:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7943,11 +7943,11 @@ else if (isShowingText || showVi) {
                                       {(effectiveMode === 'full_sentence' && !showHint && status !== 'retyping' && !(status === 'correct' && isAutoReview)) ? (
                                             <span className="text-zinc-300 font-sans tracking-widest">＿＿＿＿＿＿＿＿＿＿＿＿</span>
                             ) : effectiveMode === 'full_sentence' ? (
-                                            /* ĐÃ FIX: Thêm inline-block để Furigana không bị cắt xén lề trên */
-                                            <span className="font-sans leading-loose text-zinc-900 inline-block mt-2">
-                                                {renderFurigana(currentItem.sentence, true)}
-                                            </span>
-                                        ) : (
+           
+            <div className="font-sans leading-loose text-zinc-900 w-full pt-2 break-words">
+                {renderFurigana(currentItem.sentence, true)}
+            </div>
+        ) : (
                                             /* CÂU ĐỤC LỖ BÂY GIỜ SẼ BẢO TOÀN 100% FURIGANA */
                                             renderMaskedSentence(
                                                 currentItem.sentence, 
